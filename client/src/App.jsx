e48b08bf2efb763e69d8c2e737558ee544142c5b
Usama <usamamukhtar.work@gmail.com>
2024-10-01 23:37:00 +0500

    Install react js, tailwindcss and creates first tempelate

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <div className='text-stone-400 font-semibold text-3xl'>This is mern Blog project</div>
+    </div>
+  )
+}
