370f5720d5a991499ba2a0499fa5acd1ef5150b5
Usama <usamamukhtar.work@gmail.com>
2024-10-03 01:13:41 +0500

    Created SignIn API Route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,10 @@
 import User from "../models/user.models.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();  // This loads environment variables from the .env file
+
 
 export const signup = async (req, res, next) =>{
 
@@ -27,3 +31,28 @@ export const signup = async (req, res, next) =>{
         next(error)
     }
 } 
+
+export const signin = async (req, res, next)=>{
+    const {email, password} = req.body;
+
+    if (!email || !password || email==='' || password===''){
+        return next(errorHandler(400, 'All fields are menddatory'))
+    }
+
+    try{
+        const validUser = await User.findOne({email});
+        if (!validUser){
+            return next(errorHandler(404, 'User Not Found'))
+        }
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        const {password: pass, ...rest } =validUser._doc;
+
+        if(!validPassword){
+            return next(errorHandler(400, 'Invalid Password'))
+        }
+        const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET);
+        res.status(200).cookie('access_token', token, {httpOnly: true}).json(rest);
+    }catch(error){
+        return next(error)
+    }
+}
