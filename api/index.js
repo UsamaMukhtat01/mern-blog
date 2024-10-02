4c60a49973cdf25aadb2ae12a93464c765c9bbb2
Usama <usamamukhtar.work@gmail.com>
2024-10-02 17:37:45 +0500

    Create Signup API Route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 mongoose.connect('mongodb://localhost:27017/').then(()=>{
     console.log('Database connected successfully')
@@ -11,8 +12,11 @@ mongoose.connect('mongodb://localhost:27017/').then(()=>{
 
 const app = express();
 
+app.use(express.json())
+
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!')
 })
 
 app.use('/api/user', userRoutes)
+app.use('/api/auth', authRoutes)
