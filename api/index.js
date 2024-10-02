b441d5af2a9a22575a078cba63108b3520d0155d
Usama <usamamukhtar.work@gmail.com>
2024-10-02 16:04:13 +0500

    Connected to DataBase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,12 @@
 import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/').then(()=>{
+    console.log('Database connected successfully')
+}).catch(err =>{
+    console.log(err)
+})
+
 
 const app = express();
 
