f359569ffb0caa606531cb13ba96bc3245cdb6b2
Usama <usamamukhtar.work@gmail.com>
2024-10-02 15:49:03 +0500

    Create and run the server

--- client/src/components/Headers.jsx
+++ client/src/components/Headers.jsx
@@ -5,51 +5,49 @@ import { FaMoon } from 'react-icons/fa';
 
 export default function Headers() {
   const path = useLocation().pathname;
+
   return (
-    <Navbar className='border-b-2'>
-        <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
-          <span className='px-2 py-1 bg-gradient-to-br from-green-400 to-blue-600 rounded-lg text-white'>
-            Usama's
-          </span>
-          <span>Blog</span>
-        </Link>
-        <form>
-          <TextInput
-            type='text'
-            placeholder='Search...'
-            rightIcon={AiOutlineSearch}
-            className='hidden lg:inline'
-          />
-        </form>
-        <Button className='w-12 h-10 lg:hidden' color='red' pill>
+    <Navbar className="border-b-2" fluid rounded>
+      <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
+        <span className="px-2 py-1 bg-gradient-to-br from-green-400 to-blue-600 rounded-lg text-white">Usama's</span>
+        <span>Blog</span>
+      </Link>
+
+      <form>
+        <TextInput
+          type="text"
+          placeholder="Search..."
+          rightIcon={AiOutlineSearch}
+          className="hidden lg:inline"
+        />
+      </form>
+
+      <Button className="w-12 h-10 lg:hidden" color="red" pill>
         <AiOutlineSearch />
+      </Button>
+
+      <div className="flex gap-2 px-10 md:order-2">
+        <Button className="w-12 h-10 hidden sm:inline" color="red" pill>
+          <FaMoon />
         </Button>
-        <div className='flex gap-2 px-10 md:order-2'>
-          <Button className='w-12 h-10 hidden sm:inline' color='red' pill>
-            <FaMoon/>
-          </Button>
-          <Link to='sign-in'>
-          <Button className='bg-gradient-to-r from-cyan-500 to-blue-500'>Sign In</Button>
-          </Link>
-          <Navbar.Toggle />
-        </div>
-        <Navbar.Collapse>
-          <Navbar.Link active={path === '/'} as={'div'}>
-            <Link to='/'>
-              Home
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === 'projects'} as={'div'}>
-            <Link to='/projects'>
-              Projects
-            </Link>
-          </Navbar.Link>
-          <Navbar.Link active={path === 'about'} as={'div'}>
-            <Link to='/about'>
-              About
-            </Link>
-          </Navbar.Link>
-        </Navbar.Collapse>
+        <Link to="sign-in">
+          <Button className="bg-gradient-to-r from-cyan-900 to-blue-900">Sign In</Button>
+        </Link>
+        <Navbar.Toggle />
+      </div>
+
+      {/* Make sure the links are visible */}
+      <Navbar.Collapse className="md:flex md:items-center md:space-x-4">
+        <Navbar.Link className={`${path==='/'? 'text-blue-600' : ''}`} active={path === "/"} as={"div"}>
+          <Link to="/">Home</Link>
+        </Navbar.Link>
+        <Navbar.Link className={`${path==='/projects'? 'text-blue-600' : ''}`} active={path === "/projects"} as={"div"}>
+          <Link to="/projects">Projects</Link>
+        </Navbar.Link>
+        <Navbar.Link className={`${path==='/about'? 'text-blue-600' : ''}`} active={path === "/about"} as={"div"}>
+          <Link to="/about">About</Link>
+        </Navbar.Link>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
